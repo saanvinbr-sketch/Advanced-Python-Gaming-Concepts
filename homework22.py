c7a8655d409cfd339203795bfbcc9a93b20dbe16
saanvinbr-sketch <saanvinbr@gmail.com>
2025-12-12 17:42:46 -0600

    Add files via upload

--- homework22.py
+++ homework22.py
@@ -1,6 +1,7 @@
 import pygame
 from Paddle import Paddle
 from Ball import Paddle_Ball
+from bricks import Bricks
 pygame.init()
 
 WIDTH=1200
@@ -17,9 +18,27 @@ paddle_group=pygame.sprite.Group()
 paddle=Paddle()
 paddle_group.add(paddle)
 
+bricks_group=pygame.sprite.Group()
+
 FPS=30
 clock=pygame.time.Clock()
 gameloop=True
+
+total_rows = 7
+total_columns = 7
+
+start_x=200
+start_y=20
+
+x_space=100
+y_space=40
+
+for r in range(0,total_rows):
+    for c in range(0,total_columns):
+        x = start_x + (c * x_space)
+        y = start_y + (r * y_space)
+        b = Bricks(x,y,bricks_group)
+        bricks_group.add(b)
 
 while gameloop:
     for event in pygame.event.get():
@@ -32,8 +51,11 @@ while gameloop:
     paddle_ball_group.draw(screen)
 
     paddle_group.update(event)
-
     paddle_group.draw(screen)
+
+    bricks_group.update()
+    bricks_group.draw(screen)
+
     pygame.display.flip()
     clock.tick(FPS)
 
